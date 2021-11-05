e9a22649286d3f174efe07f8545912bf736f1328
Andronis Adamantios <adamantios.andronis@outlook.com.gr>
2021-11-05 15:38:01 +0000

    initial MoreInputKeys 1.0.0

---
+++ MoreInputKeys/Binding_isValidKey_Patch.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using HarmonyLib;
+using UnityEngine;
+
+namespace MoreInputKeys
+{
+    [HarmonyPatch(typeof(MadrugaShared.Binding), "isValidKey", MethodType.Normal)]
+    static class Binding_isValidKey_Patch
+    {
+
+        [HarmonyPrefix]
+        static bool Prefix(ref bool __result, KeyCode code)
+        {
+            __result = isValidKey(code);
+            return false;
+        }
+
+        private static bool isValidKey(KeyCode code)
+        {
+            switch (code)
+            {
+                case KeyCode.None:
+                case KeyCode.Clear:
+                case KeyCode.Pause:
+                case KeyCode.Escape:
+                case KeyCode.F9:
+                case KeyCode.F10:
+                case KeyCode.F11:
+                case KeyCode.F12:
+                case KeyCode.F13:
+                case KeyCode.F14:
+                case KeyCode.F15:
+                case KeyCode.Numlock:
+                case KeyCode.CapsLock:
+                case KeyCode.ScrollLock:
+                case KeyCode.RightShift:
+                case KeyCode.LeftShift:
+                case KeyCode.RightControl:
+                case KeyCode.LeftControl:
+                case KeyCode.RightAlt:
+                case KeyCode.LeftAlt:
+                case KeyCode.RightCommand:
+                case KeyCode.LeftCommand:
+                case KeyCode.LeftWindows:
+                case KeyCode.RightWindows:
+                case KeyCode.AltGr:
+                case KeyCode.Help:
+                case KeyCode.Print:
+                case KeyCode.SysReq:
+                case KeyCode.Break:
+                case KeyCode.Menu:
+                    return false;
+                default:
+                    return true;
+            }
+        }
+
+    }
+}
--- MoreInputKeys/Properties/AssemblyInfo.cs
+++ MoreInputKeys/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("TestMod")]
+[assembly: AssemblyTitle("MoreInputKeys")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("TestMod")]
+[assembly: AssemblyProduct("MoreInputKeys")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("7996e6a0-1921-4923-9748-3273ae186298")]
+[assembly: Guid("4e76da82-7efc-45dd-8c6b-5ad3248c8697")]
 
 // Version information for an assembly consists of the following four values:
 //
